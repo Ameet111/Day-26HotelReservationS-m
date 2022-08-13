5899ac8f07590a63cfa9d7be3c75ffdf8d73b264
Ameet Jadhav <ammugs11@gmail.com>
2022-08-13 14:32:17 +0530

    added the use case 2 ability to find the chepest hotel is added

---
+++ src/com/bridgelabz/hotelreservation1/Hotel.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.hotelreservation1;
+
+public class Hotel {
+	
+	 private String hotelName;
+	    private int totalRate;
+
+	    public String getHotelName() {
+	        return hotelName;
+	    }
+
+	    public void setHotelName(String hotelName) {
+	        this.hotelName = hotelName;
+	    }
+
+	    public int getTotalRate() {
+	        return totalRate;
+	    }
+
+	    public void setTotalRate(int totalRate) {
+	        this.totalRate = totalRate;
+	    }
+
+	    @Override
+		public String toString() {
+	    	System.out.println("Hotel with cheapest rates are : ");
+			return	" Hotel name : " + hotelName + "  "+
+					" Hotel total rates : " + totalRate ;
+
+		}
+
+}
---
+++ src/com/bridgelabz/hotelreservation1/HotelInformation.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.hotelreservation1;
+
+public class HotelInformation {
+	
+	private String hotelName;
+	private int hotelRates;
+	private int totalRate;
+	public String getName() {
+		return hotelName;
+	}
+
+	public void setName(String name) {
+		this.hotelName = name;
+	}
+
+	public int getRates() {
+		return hotelRates;
+	}
+
+	public void setRates(int rates) {
+		this.hotelRates = rates;
+	}
+
+	public int getTotalRate(int daysBetween) {
+		totalRate = hotelRates * (daysBetween+1);
+		return totalRate;
+	}
+
+	public void setTotalRate(int totalRate) {
+		this.totalRate = totalRate;
+	}
+
+	@Override
+	public String toString() {
+		return	"\n Hotel name : " + hotelName + 
+				"\n Hotel Rates : " + hotelRates ;
+
+	}
+
+
+}
