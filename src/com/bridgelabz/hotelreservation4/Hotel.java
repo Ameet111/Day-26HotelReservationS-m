9f55ea7649747f42728e9069adbf48a492de1a53
Ameet Jadhav <ammugs11@gmail.com>
2022-08-13 15:01:37 +0530

    added the use case 5 add hotel rating is added

---
+++ src/com/bridgelabz/hotelreservation4/Hotel.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.hotelreservation4;
+
+public class Hotel {
+	
+	 private String hotelName;
+	    private int totalRate;
+
+	    public String getHotelName() {
+	        return hotelName;
+	    }
+
+	    public void setHotelName(String hotelName) {
+	        this.hotelName = hotelName;
+	    }
+
+	    public int getTotalRate() {
+	        return totalRate;
+	    }
+
+	    public void setTotalRate(int totalRate) {
+	        this.totalRate = totalRate;
+	    }
+
+	    @Override
+		public String toString() {
+			return	" Hotel name : " + hotelName + "  "+
+					" Hotel total rates : " + totalRate ;
+			
+	    }
+
+
+}
