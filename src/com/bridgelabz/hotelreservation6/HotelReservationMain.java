dd1d7c2b14716b2e834a5b7a52aa321c19b8813b
Ameet Jadhav <ammugs11@gmail.com>
2022-08-13 15:23:23 +0530

    added find the hotel with best rating is added

---
+++ src/com/bridgelabz/hotelreservation6/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservation6;
+
+public class HotelReservationMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program ");
+	}
+
+}
